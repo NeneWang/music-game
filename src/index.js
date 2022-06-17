18bb6a0d0481e7360ae52d7e5f392de456ffb016
NelsonWang <wangnelson2@gmail.com>
2022-06-17 16:30:00 -0400

    funcitonal version

--- src/index.js
+++ src/index.js
@@ -1,94 +1,104 @@
 
 import Phaser from "phaser";
-debugger
-class Example extends Phaser.Scene
-{
-    constructor ()
-    {
-        super();
+class Example extends Phaser.Scene {
+  constructor() {
+    super();
+  }
+
+  preload() {
+    this.load.image('title', 'assets/pics/gms.png');
+
+    this.load.spritesheet('button', 'assets/ui/flixel-button.png', { frameWidth: 80, frameHeight: 20 });
+
+    this.load.bitmapFont('nokia', 'assets/fonts/bitmapFonts/nokia16black.png', 'assets/fonts/bitmapFonts/nokia16black.xml');
+
+    this.load.audioSprite('sfx', 'assets/audio/SoundEffects/fx_mixdown.json', [
+      'assets/audio/SoundEffects/fx_mixdown.ogg',
+      'assets/audio/SoundEffects/fx_mixdown.mp3'
+    ]);
+  }
+
+  create() {
+    
+    this.add.image(config.width / 2, config.height / 2, 'title').setScale(.23);
+
+    // this.add.image(400, 300, 'title');
+
+    const spritemap = this.cache.json.get('sfx').spritemap;
+
+    console.log(spritemap)
+    this.sound.playAudioSprite('sfx', 'boss hit');
+    // this.makeButton('mew', 680, 115 + i * 40);
+
+
+    let i = 0;
+    for (let spriteName in spritemap) {
+      if (!spritemap.hasOwnProperty(spriteName)) {
+        continue;
+      }
+      this.makeButton(spriteName, 680, 115 + i * 40);
+      console.log("Here")
+      i++;
     }
 
-    preload ()
-    {
-        this.load.image('title', 'assets/pics/catastrophi.png');
+    this.input.on('gameobjectover', function (pointer, button) {
+      this.setButtonFrame(button, 0);
+    }, this);
 
-        this.load.spritesheet('button', 'assets/ui/flixel-button.png', { frameWidth: 80, frameHeight: 20 });
+    this.input.on('gameobjectout', function (pointer, button) {
+      this.setButtonFrame(button, 1);
+    }, this);
 
-        this.load.bitmapFont('nokia', 'assets/fonts/bitmap/nokia16black.png', 'assets/fonts/bitmap/nokia16black.xml');
+    this.input.on('gameobjectdown', function (pointer, button) {
+      this.sound.playAudioSprite('sfx', button.name);
+      this.setButtonFrame(button, 2);
+    }, this);
 
-        this.load.audioSprite('sfx', 'assets/audio/SoundEffects/fx_mixdown.json', [
-            'assets/audio/SoundEffects/fx_mixdown.ogg',
-            'assets/audio/SoundEffects/fx_mixdown.mp3'
-        ]);
-    }
+    this.input.on('gameobjectup', function (pointer, button) {
+      this.setButtonFrame(button, 0);
+    }, this);
 
-    create ()
-    {
-        this.add.image(400, 300, 'title');
+  }
 
-        const spritemap = this.cache.json.get('sfx').spritemap;
+  makeButton(name, x, y) {
+    const button = this.add.image(x, y, 'button', 1)
+      .setInteractive();
+    button.name = name;
+    button.setScale(2, 1.5);
 
-        let i = 0;
-        for (let spriteName in spritemap)
-        {
-            if (!spritemap.hasOwnProperty(spriteName))
-            {
-                continue;
-            }
-            this.makeButton(spriteName, 680, 115 + i*40);
-            i++;
-        }
+    const text = this.add.bitmapText(x - 40, y - 8, 'nokia', name, 16);
+    text.x += (button.width - text.width) / 2;
+  }
 
-        this.input.on('gameobjectover', function (pointer, button)
-        {
-            this.setButtonFrame(button, 0);
-        }, this);
+  makeButton2(name, x, y) {
+    const button = this.add.image(x, y, 'button', 1)
+      .setInteractive();
+    button.name = name;
+    button.setScale(2, 1.5);
 
-        this.input.on('gameobjectout', function (pointer, button)
-        {
-            this.setButtonFrame(button, 1);
-        }, this);
+    const text = this.add.bitmapText(x - 40, y - 8, 'nokia', name, 16);
+    text.x += (button.width - text.width) / 2;
+  }
 
-        this.input.on('gameobjectdown', function (pointer, button)
-        {
-            this.sound.playAudioSprite('sfx', button.name);
-            this.setButtonFrame(button, 2);
-        }, this);
+  setButtonFrame(button, frame) {
+    button.frame = button.scene.textures.getFrame('button', frame);
+  }
 
-        this.input.on('gameobjectup', function (pointer, button)
-        {
-            this.setButtonFrame(button, 0);
-        }, this);
-
-    }
-
-    makeButton(name, x, y)
-    {
-        const button = this.add.image(x, y, 'button', 1)
-            .setInteractive();
-        button.name = name;
-        button.setScale(2, 1.5);
-
-        const text = this.add.bitmapText(x - 40, y - 8, 'nokia', name, 16);
-        text.x += (button.width - text.width) / 2;
-    }
-
-    setButtonFrame(button, frame)
-    {
-        button.frame = button.scene.textures.getFrame('button', frame);
-    }
+  setButtonFrame2(button, frame) {
+    button.frame = button.scene.textures.getFrame('button', frame);
+  }
 }
 
 const config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: [ Example ],
-    pixelArt: true,
-    audio: {
-        disableWebAudio: true
-    }
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 700,
+  height: 700,
+  scene: [Example],
+  pixelArt: true,
+  audio: {
+    disableWebAudio: true
+  }
 };
 
 new Phaser.Game(config);
